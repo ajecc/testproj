8f515faa46e837cf3b4e41958353d12b673f5aa7
Eugen Ajechiloae <eugenajechiloae@gmail.com>
2025-12-05 17:18:32 +0200

    Update testfile.py

--- src/myproj/testfile.py
+++ src/myproj/testfile.py
@@ -2,6 +2,7 @@ import argparse
 from clearml import Task
 
 if __name__ == "__main__":
+    print("are we local???", Task.running_locally())
     task = Task.init(project_name="testproj", task_name="testfile", reuse_last_task_id=False)
     p = argparse.ArgumentParser()
     p.add_argument("config", type=argparse.FileType("r"))
